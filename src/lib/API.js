6312d649bfd51d9c185b074bba394cb8c8965083
Jacob Reilly-Cooper <jacob@credersi.com>
2022-02-02 11:30:31 +0000

    Environment Variable Information added

--- src/lib/API.js
+++ src/lib/API.js
@@ -1,11 +1,14 @@
 const fetchMovieData = () => {
     return new Promise((resolve, reject) => {
-        fetch("http://www.omdbapi.com/?t=Avengers&apikey=")
+        fetch(`http://www.omdbapi.com/?t=Avengers&apikey=INSERTKEYHERE`)
             .then(res => res.json())
             .then(data => resolve(data))
             .catch(err => reject(err))
     })
 }
+
+// Environment Variables preferred when using API key. API key can be accessed via: http://www.omdbapi.com/apikey.aspx
+// Environment Variable documentation located here: https://lightningjs.io/docs/#/lightning-cli-reference/EnvironmentVariables?id=environment-variables
 
 async function getMovieData() {
     const data = await fetchMovieData()
